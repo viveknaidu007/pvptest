1354c571fc01b3cd8978b43b79dcf8d221e219ba
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-11-30 21:33:56 +0530

    "Url update"

--- dist/model/CustomerModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { model } = require("mongoose");
-const { CustomerSchema } = require("../schemas/customerSchema");
-const CustomerModel = new model("customer", CustomerSchema);
-module.exports = { CustomerModel };
